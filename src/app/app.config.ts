1888d32eafced06d7ab4cb9538dff14c2aef854c
wickerknox <wickerknox89@gmail.com>
2024-10-08 15:45:12 -0700

    Add @ngrx/store and @ngrx/effects to config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,9 +11,11 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideStore({}),
+    provideEffects(),
     provideStoreDevtools({
       maxAge: 3, // Retains last 3 states
-      // logOnly: !isDevMode(), // Restrict extension to log-only mode
+      //logOnly: !isDevMode(), // Restrict extension to log-only mode
       autoPause: true, // Pauses recording actions and state changes when the extension window is not open
       trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
       traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
