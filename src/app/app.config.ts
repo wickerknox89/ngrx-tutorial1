b932888d78a45d73ffa302b80e8f51fa05f6b814
wickerknox <wickerknox89@gmail.com>
2024-10-08 15:26:57 -0700

    Add @ngrx/store-devtools configurations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,22 @@ import { ApplicationConfig, provideZoneChangeDetection, provideExperimentalZonel
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
- 
+    provideStoreDevtools({
+      maxAge: 3, // Retains last 3 states
+      // logOnly: !isDevMode(), // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+      connectInZone: true // If set to true, the connection is established within the Angular zone
+    })
   ]
 };
