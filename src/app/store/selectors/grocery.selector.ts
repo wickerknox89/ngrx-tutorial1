8e4f6d5aedf952c63f005c7f18d24bf9f6702b7f
wickerknox <wickerknox89@gmail.com>
2024-10-09 14:09:41 -0700

    Create selector using  'createFeatureSelector' convenience method

--- src/app/store/selectors/grocery.selector.ts
+++ src/app/store/selectors/grocery.selector.ts
@@ -1,4 +1,7 @@
+import { createFeatureSelector, createSelector } from "@ngrx/store";
 import { Grocery } from "../../../models/grocery.model";
 
-export const selectGroceries = (state:{groceries:Grocery[]})=>state.groceries
+//export const selectGroceries = (state:{groceries:Grocery[]})=>state.groceries
+export const selectGroceries = createFeatureSelector<Grocery[]>('groceries') 
+
 
