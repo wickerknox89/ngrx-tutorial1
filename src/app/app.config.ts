ed893f2550cda8f5da71fdee73623f4d874b2516
wickerknox <wickerknox89@gmail.com>
2024-10-09 00:28:03 -0700

    Revert config to storing 25 states

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -19,7 +19,7 @@ export const appConfig: ApplicationConfig = {
     }),
     provideEffects(),
     provideStoreDevtools({
-      maxAge: 3, // Retains last 3 states
+      maxAge: 25, // Retains last 25 states
       //logOnly: !isDevMode(), // Restrict extension to log-only mode
       autoPause: true, // Pauses recording actions and state changes when the extension window is not open
       trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
